d6829fdb8b8fbacc47ccbae0a46ed92dd706dc6b
fazhao <448517683@qq.com>
2016-08-28 16:11:07 +0800

    fix drawText can't display text bugs,and now the text can display in center

--- app/src/main/java/com/example/apple/myapplication/customeview/OperationView.java
+++ app/src/main/java/com/example/apple/myapplication/customeview/OperationView.java
@@ -3,6 +3,7 @@ package com.example.apple.myapplication.customeview;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -34,36 +35,79 @@ public class OperationView extends View {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 //        return super.onTouchEvent(event);
-        switch (event.getAction()){
+        switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                MainActivity.xy.setText(""+event.getRawX()+","+event.getRawY());
+                MainActivity.xy.setText("" + event.getRawX() + "," + event.getRawY());
                 break;
             case MotionEvent.ACTION_MOVE:
-                MainActivity.xy.setText(""+event.getRawX()+","+event.getRawY());
+                MainActivity.xy.setText("" + event.getRawX() + "," + event.getRawY());
                 break;
             case MotionEvent.ACTION_UP:
-                MainActivity.xy.setText(""+event.getRawX()+","+event.getRawY());
+                MainActivity.xy.setText("" + event.getRawX() + "," + event.getRawY());
                 break;
         }
         return true;
     }
 
+    /**
+     * 获取字体高度
+     *
+     * @param textSize : textSize==null时  获取字符串的默认字体,
+     */
+    public static int getFontHeight(Integer textSize) {
+        Paint paint = new Paint();
+        if (textSize != null) {
+            paint.setTextSize(textSize);
+        }
+        Paint.FontMetrics fm = paint.getFontMetrics();
+        return (int) (fm.descent - fm.ascent);
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
+        /**
+         * the canvas drawColor before onDraw will not cover the text!
+         * */
+        canvas.drawColor(getResources().getColor(R.color.colorPrimary));
         super.onDraw(canvas);
         float x = this.getX();
         float y = this.getY();
         int w = this.getWidth();
         int h = this.getHeight();
-        Log.e("x",String.valueOf(x));
+        Log.e("x", String.valueOf(x));
         Log.e("y", String.valueOf(y));
-        Log.e("x+w/2",String.valueOf(x+w/2));
-        Log.e("y+h/2",String.valueOf(y+h/2));
+        Log.e("x+w/2", String.valueOf(x + w / 2));
+        Log.e("y+h/2", String.valueOf(y + h / 2));
         Paint paint = new Paint();
+        Rect rect = new Rect();
+        paint.setTextAlign(Paint.Align.LEFT);
+        paint.setColor(getResources().getColor(R.color.white));
+        paint.getTextBounds("click to scale", 0 , "click to scale".length(), rect);
         paint.setAntiAlias(true);
-        paint.setColor(getResources().getColor(R.color.colorPrimary));
-        canvas.drawText("click to scale",x + w / 2,y + h / 2,paint);
-        canvas.drawColor(getResources().getColor(R.color.colorPrimary));
+//        paint.setColor(getResources().getColor(R.color.colorPrimary));
+        /**
+         * display1: can display is center(correct)
+         * */
+//        canvas.drawText("click to scale", 0, rect.width(), paint);
+        /**、
+         * this is wrong not display
+         * */
+//        canvas.drawText("click to scale",x+w/2,y+h/2,paint);
+
+        /**
+        * the canvas drawColor after onDraw will cover the text!
+        * */
+//        canvas.drawColor(getResources().getColor(R.color.colorPrimary));
+        /**
+         * default color is black
+         * display2: can display but not in center
+         * */
+//        canvas.drawText("click to scale",0,getFontHeight(null),paint);
+        /**
+         * default color is black
+         * display2: can display in center
+         * */
+        canvas.drawText("click to scale",(getMeasuredWidth() - rect.width()) / 2,(getMeasuredHeight() + rect.height()) / 2,paint);
 //        Rect rect = new Rect();
     }
 
@@ -77,7 +121,7 @@ public class OperationView extends View {
         super.onLayout(changed, left, top, right, bottom);
     }
 
-    public void scale(){
+    public void scale() {
 //        Matrix matrix = new Matrix();
 //        matrix.postScale(2,2);
         this.getMatrix().setScale(2, 2);
